2473546d1b18be72bed2561f7736be56b080fcef
Sem Thijssen <semthijssen@MacBook-Pro-van-Sem.local>
2020-09-25 12:19:45 +0200

    uitroepteken

--- gedicht.py
+++ gedicht.py
@@ -3,5 +3,5 @@ vers2 = " Toen ik werd geboren begon ik te krijsen."
 vers3 = " Later wil ik werken bij walt disney."
 vers4 = " Maar zonder opleiding gaat dat niet."
 vers5 = " ik hoop dat ik de opleiding ga afronden."
-vers6 = " Want daarmee kan ik naar de volgende ronde."
+vers6 = " Want daarmee kan ik naar de volgende ronde!."
 print(vers1,vers2,vers3,vers4,vers5,vers6,sep="--")
