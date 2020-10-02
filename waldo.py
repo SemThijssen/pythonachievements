5e39d48cf97eea39ff8be3dffe578065816751cb
Sem Thijssen <semthijssen@MacBook-Pro-van-Sem.local>
2020-10-02 13:01:12 +0200

    ik Heb Waldoopdracht gedaan.

---
+++ waldo.py
@@ -0,0 +1,10 @@
+import random
+people = ["Waldo","Jackson","Ray","Eric","Vaughn","Jesse","Herbert", "Rodrigo","Elija","Sasha","Nathaniel","Ellie","Allison","Jeremiah", "Paula", "Alisha", "Tory","Troy", "Faye"]
+random.shuffle(people)
+for Waldo in people:
+    
+    if Waldo == "Waldo":
+        print(Waldo)
+        print (people.index("Waldo"))
+print(people)
+        
